b0e294353f3e622b3eaeb89a8efe2d55121ec655
Helene Lemascon <lemasson.h@gmail.com>
2018-03-08 14:13:35 +0000

    Add functionality to delete ingredient

--- src/containers/Admin/AdminIngredients.js
+++ src/containers/Admin/AdminIngredients.js
@@ -13,12 +13,24 @@ class AdminIngredients extends Component {
     this.props.history.push('/admin/ingredients/' + ingredientId + '/edit');
   }
 
+  deleteIngredientHandler = (ingredientId) => {
+    this.props.onDeleteIngredient(ingredientId, this.props.token);
+  }
+
+  componentDidUpdate() {
+    if (this.props.delete_finished) {
+      this.props.onDeleteIngredientReset();
+      this.props.onLoadIngredients();
+    }
+  }
+
   render() {
     return (
       <AdminList
         entityName='ingredients'
         entityAddLink="/admin/ingredients/add"
         editEntity={this.editIngredientHandler}
+        deleteEntity={this.deleteIngredientHandler}
         entityList={this.props.ingredients}
         loading={this.props.loading}
         error={this.props.error} />
@@ -31,12 +43,16 @@ const mapStateToProps = state => {
     loading: state.ingredients.ingredients_loading,
     ingredients: state.ingredients.ingredients,
     error: state.ingredients.ingredients_error,
+    delete_finished: state.adminIngredient.delete.finished,
+    token: state.auth.token,
   };
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     onLoadIngredients: () => dispatch(actionCreators.loadIngredients()),
+    onDeleteIngredient: (ingredientId, token) => dispatch(actionCreators.adminDeleteIngredient(ingredientId, token)),
+    onDeleteIngredientReset: () => dispatch(actionCreators.adminDeleteIngredientReset()),
   }
 }
 

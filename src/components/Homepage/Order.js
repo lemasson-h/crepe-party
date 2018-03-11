f734b63b287eebef88d8d114ac4b9ac42eccb6d1
Helene Lemascon <lemasson.h@gmail.com>
2018-03-11 16:35:00 +0000

    Rename Command by Order

---
+++ src/components/Homepage/Order.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import DeleteImage from '../UI/DeleteImage';
+import EditImage from '../UI/EditImage';
+import { getIngredientNames } from '../../helpers/crepeHelper';
+
+const order = (props) => {
+  const length = props.orders.length;
+  let currentLength = 0;
+  let orders = props.orders.map(order => {
+    ++currentLength;
+    const ingredientNames = getIngredientNames(order, props.ingredients);
+
+    return (
+      <Aux key={order.uniqueId}>
+        <div className="Element">
+          <div className="Title">{order.name}</div>
+          <div className="Content">
+            <div>{ingredientNames.join(', ')}</div>
+            <div className="Actions">
+              <button className="ImageButton" onClick={(e) => props.openModalMethod(e, order)}><EditImage message="Edit crepe" /></button>
+              <button className="ImageButton" onClick={(e) => props.deleteCrepeMethod(e, order.uniqueId)}><DeleteImage message="Delete crepe" /></button>
+            </div>
+          </div>
+        </div>
+        {currentLength < length ? <div className="LineSeparator" /> : null}
+      </Aux>
+    );
+  });
+
+  if (orders.length === 0) {
+    orders = <div className="EmptyBasket">Empty basket.</div>
+  }
+
+  return (
+    <div className="Order">
+      <h1 className="Header">My order</h1>
+      {orders}
+      <div className="Actions">
+        <button className="Submit">Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default order;
